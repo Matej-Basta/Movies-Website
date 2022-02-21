9b2133015f7eed9ac549caa46fd55022acc555dc
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-21 20:56:29 +0100

    shawshank redemption

--- resources/views/movies/detail.blade.php
+++ resources/views/movies/detail.blade.php
@@ -23,6 +23,114 @@
 
     </ul>
 
+    <h2>People</h2>
+    <div style="display:flex">
+    <h3>Cast</h3>
+    <ul>
+        <?php foreach($cast as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+
+    <h3>Director</h3>
+    <ul>
+        <?php foreach($director as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+
+    <h3>Writer</h3>
+    <ul>
+        <?php foreach($writer as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+    <h3>Producer</h3>
+    <ul>
+        <?php foreach($producer as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+    <h3>Composer</h3>
+    <ul>
+        <?php foreach($composer as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+    <h3>Cinematographer</h3>
+    <ul>
+        <?php foreach($cinematographer as $person) : ?>
+
+            <li>
+                <strong>Name:</strong>
+                <?= $person->fullname ?>
+                <br>
+                <strong>Description:</strong>
+                <?= $person->description ?>
+                <br>
+                <strong>Position:</strong>
+                <?= $person->slug ?>
+            </li>
+
+        <?php endforeach; ?>
+    </ul>
+    </div>
+
     <a href="/">Home</a>
     
 </body>
